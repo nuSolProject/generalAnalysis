d0f28470ce9cfecf9b75bcb167ad0a5e3565cd07
brookshartsock <137226807+brookshartsock@users.noreply.github.com>
2026-09-08 12:02:46 -0500

    Update RPBinDecoderV1.cc

--- RPBinDecoder/RPBinDecoderV1.cc
+++ RPBinDecoder/RPBinDecoderV1.cc
@@ -1,3 +1,4 @@
+//general C++ includes
 #include <iostream>
 #include <vector>
 #include <array>
@@ -19,11 +20,12 @@
 #include "TFile.h"
 #include "TError.h"
 
-const int eventsPerFile = 1e5;//1e5
-const int samplesPerEvent = 1024;
-double RPCHData[eventsPerFile][2][samplesPerEvent];
-bool savePngs = 0;
+const int eventsPerFile = 1e5; //number of events in the .bin file
+const int samplesPerEvent = 1024; //samples taken for every event (1024 for now)
+double RPCHData[eventsPerFile][2][samplesPerEvent]; //massive data array of all events from .bin file (empty right now) 
+bool savePngs = 0; //options to save fit events as pngs
 
+//output data structure for usual analysis
 struct outData{
   std::string timeStamp;
   double fullInt1;
@@ -40,14 +42,7 @@ struct outData{
     fullInt2(fI2), trigTime2(tT2), smartInt2(sI2) {}
 };
 
-/*
-  f1.SetParameter(0, p0Guess1);
-  f1.SetParameter(1, tauRiseGuess);
-  f1.SetParameter(2, tauDecay1Guess);
-  f1.SetParameter(3, p0Guess1/6.);
-  f1.SetParameter(4, tauDecay2Guess);
-  f1.SetParameter(5, triggerCH1Guess);
-  f1.SetParameter(6, 0.0);*/
+//output structure for fit data
 struct fitData{
   double ampFastCH1;
   double tauRiseCH1;
@@ -73,10 +68,11 @@ struct fitData{
 };
 
 
-
-std::vector<outData> outDataList;
+//output data vectors made from our structures
+std::vector<outData> outDataList; //these should probably be arrays? with a fixed length like the input data array...
 std::vector<fitData> fitDataList;
 
+//binary magic please disregard
 static std::string nsToIso8601(uint64_t ns_since_epoch) {
   time_t sec = static_cast<time_t>(ns_since_epoch / 1000000000ull);
   uint32_t nsec = static_cast<uint32_t>(ns_since_epoch % 1000000000ull);
@@ -92,6 +88,7 @@ static std::string nsToIso8601(uint64_t ns_since_epoch) {
   return oss.str();
 }
 
+//opens the binary file, decodes the time stamp and waveform data event by event, stores it into RPCHData array
 bool readStoreData(std::string curFilename){
 #pragma pack(push,1)
   struct FileHeader {
@@ -160,6 +157,7 @@ bool readStoreData(std::string curFilename){
 }
 
 
+//test function to see if data makes sense
 void coutRPCHData(){
 
   std::cout << outDataList[0].timeStamp << ":\n";
@@ -171,6 +169,7 @@ void coutRPCHData(){
 }
 
 
+//integrates entire 1024 sample event and stores in outDataList
 void computeFullInts(){
 
 
@@ -201,6 +200,7 @@ void computeFullInts(){
 }
 
 
+//tester function
 void coutFullInts(){
 
 
@@ -213,6 +213,7 @@ void coutFullInts(){
 }
 
 
+//peak finding algorithm for each event stored into outDataList
 int findPeakIndex(int curEvent, int channel){
 
 
@@ -233,6 +234,7 @@ int findPeakIndex(int curEvent, int channel){
 }
 
 
+//trigger estimating algorithm stored to outDataList
 int findTrigIndex(int curEvent, int channel){
 
 
@@ -257,11 +259,12 @@ int findTrigIndex(int curEvent, int channel){
 }
 
 
+//computer shorter integration window fixed at software trigger from findTrigIndex
 void computeSmartInts(){
 
 
   int preSamples = 8;
-  int totalSamples = 512;
+  int totalSamples = 256;
   
   for(int eventNo = 0; eventNo < eventsPerFile; ++eventNo){
 
@@ -295,7 +298,7 @@ void computeSmartInts(){
 
     }
 
-    outDataList[eventNo].smartInt2 = fullInt;
+    outDataList[eventNo].smartInt2 = fullInt; //you guessed it, stored to outDataList
     
   }
   
@@ -303,6 +306,7 @@ void computeSmartInts(){
 }
 
 
+//tester again
 void coutSmartInts(){
 
 
@@ -315,9 +319,7 @@ void coutSmartInts(){
 }
 
 
-
-
-
+//outputs all data in outDataList and fitDataList into .root file
 void storeDataInNtuple(std::string outFilename, const std::vector<outData>& dataList, const std::vector<fitData>& fitList){
 
   
@@ -352,49 +354,74 @@ void storeDataInNtuple(std::string outFilename, const std::vector<outData>& data
 }
 
 
+//everything related to fitting the waveforms with root
 void computeFitEvent(int eventNo){
 
+	//root appears to favor vectors here... converting our arrays into vectors
   std::vector<double> RPTimeVec(samplesPerEvent);
   std::vector<double> RPCH1DataVec(samplesPerEvent);
   std::vector<double> RPCH2DataVec(samplesPerEvent);
 
+	//fill our new empty vectors with data from our RPCHData array
   for(int i = 0; i < samplesPerEvent; ++i){
     RPTimeVec[i] = i*8e-9;
     RPCH1DataVec[i] = RPCHData[eventNo][0][i];
     RPCH2DataVec[i] = RPCHData[eventNo][1][i];
   }
 
-  // parameter guesses
-  int CH1PeakIndex = findPeakIndex(eventNo, 0);
-  double p0Guess1 = std::abs(RPCHData[eventNo][0][CH1PeakIndex])*2.0;
+  //initial parameter guesses
+  //we choose these to help root "find" the shape of the waveform
+  int CH1PeakIndex = findPeakIndex(eventNo, 0); //sample number that corresponds to the peak in channel 1
+  double p0Guess1 = std::abs(RPCHData[eventNo][0][CH1PeakIndex])*2.0; //we guess that p0 = 2 * peak voltage from channel 1
 
-  int CH2PeakIndex = findPeakIndex(eventNo, 1);
+  int CH2PeakIndex = findPeakIndex(eventNo, 1); //sample number that corresponds to the peak in channel 2
   double p0Guess2 = std::abs(RPCHData[eventNo][1][CH2PeakIndex])*2.0;
 
-  double p0Min = 0.0;
-  double p0Max = 20.0;
+  double p0Min = 0.0; //its reasonable to expect our peaks will be > 0 V
+  double p0Max = 20.0; //1 V is the maximum from the RP, but if we later exclude these from the fit, we could fit higher peaks
 
-  double tauRiseGuess = 45e-9;
-  double tauRiseMin   = 10e-9;
-  double tauRiseMax   = 80e-9;
+  double tauRiseGuess = 45e-9; //average rise decay time in seconds based on my detector setup for the 71As study
+  double tauRiseMin   = 10e-9; //minimum allowed rise time
+  double tauRiseMax   = 80e-9; //maximum
 
-  double tauDecay1Guess = 92e-9;
-  double tauDecay1Min   = 50e-9;
-  double tauDecay1Max   = 200e-9;
+  double tauDecay1Guess = 92e-9; //average fast decay time in seconds based on my detector setup for the 71As study
+  double tauDecay1Min   = 50e-9; //min
+  double tauDecay1Max   = 200e-9; //max
 
-  double tauDecay2Guess = 750e-9;
+  double tauDecay2Guess = 750e-9; //average slow decay time in seconds based on my detector setup for the 71As study
   double tauDecay2Min   = 500e-9;
   double tauDecay2Max   = 1000e-9;
 
+  //we base our fit "trigger" on our software trigger converted from samples to ns and shifted left by one sample
+  //the software trigger in outDataList is for a 5 mv rising edge, the true event starts before the 5 mv trigger is reached
   double triggerCH1Guess = (outDataList[eventNo].trigTime1 * 8e-9) - (8e-9);
   double triggerCH2Guess = (outDataList[eventNo].trigTime2 * 8e-9) - (8e-9);
 
+  //hard limit the fit trigger to be withing +/- 1 sample of triggerCH1Guess
   double triggerMin = -8e-9;
   double triggerMax = 8e-9;
 
+  //our root TGprah g1 will need the xmin and xmax so we take those from the time data RPTimeVec
   const double xMin = RPTimeVec.front();
   const double xMax = RPTimeVec.back();
 
+  //sorry you have to see the following string and I hope you can forgive me, let me explain:
+  //if the current sample of the event we are looking it is greater than the current estimated fit trigger:
+  //   just do the equation
+  //else (data before the trigger) the fit should be the fit baseline
+  //the equation is the same baseline added to triple-expoential function with one rising and two exponential decay components
+  //GAGG's "fast" component is about 6x larger than its "slow" component in our setup on SiPMs
+  //
+  //[0] -> amplitude of fast component based on waveform peak voltage 
+  //[1] -> rise time of waveform in s
+  //[2] -> fast decay time of waveform in s
+  //
+  //[3] -> amplitude of slow component based on 1/6 waveform peak voltage
+  //[4] -> slow decay time of waveform in s
+  //
+  //[5] -> fit trigger (start of waveform/scintilation event)
+  //[6] -> detector baseline (what the detector is reading outside of the event. usually empty noise around 0 V)
+  //
   const char *fitExpr =
     "(x>[5]) ? [6] + [0]*(exp(-(x-[5])/[2]) - exp(-(x-[5])/[1]))"
     " + [3]*(exp(-(x-[5])/[4]) - exp(-(x-[5])/[1])) : [6]";
@@ -402,10 +429,11 @@ void computeFitEvent(int eventNo){
   // -----------------------
   // CH1
   // -----------------------
-  TGraph g1(samplesPerEvent, RPTimeVec.data(), RPCH1DataVec.data());
+  TGraph g1(samplesPerEvent, RPTimeVec.data(), RPCH1DataVec.data()); //create a root graph witrh 1024 samples with xmin-xmax
 
-  TF1 f1(Form("f1_event_%d", eventNo), fitExpr, xMin, xMax);
+  TF1 f1(Form("f1_event_%d", eventNo), fitExpr, xMin, xMax); //create a root fit with our function n such
 
+  //set initial parameter guesses into our fit	
   f1.SetParameter(0, p0Guess1);
   f1.SetParameter(1, tauRiseGuess);
   f1.SetParameter(2, tauDecay1Guess);
@@ -414,6 +442,7 @@ void computeFitEvent(int eventNo){
   f1.SetParameter(5, triggerCH1Guess);
   f1.SetParameter(6, 0.0);
 
+  //set hard parameter limits into our fit
   f1.SetParLimits(0, p0Min, p0Max);
   f1.SetParLimits(1, tauRiseMin, tauRiseMax);
   f1.SetParLimits(2, tauDecay1Min, tauDecay1Max);
@@ -421,12 +450,13 @@ void computeFitEvent(int eventNo){
   f1.SetParLimits(4, tauDecay2Min, tauDecay2Max);
   f1.SetParLimits(5, triggerCH1Guess - triggerMin, triggerCH1Guess + triggerMax);
 
+  //apply the fit on to the graph of our data
   g1.Fit(&f1, "QN0", "", xMin, xMax);
 
   // -----------------------
   // CH2
   // -----------------------
-  TGraph g2(samplesPerEvent, RPTimeVec.data(), RPCH2DataVec.data());
+  TGraph g2(samplesPerEvent, RPTimeVec.data(), RPCH2DataVec.data()); //repeat for channel 2...
 
   TF1 f2(Form("f2_event_%d", eventNo), fitExpr, xMin, xMax);
 
@@ -466,14 +496,17 @@ void computeFitEvent(int eventNo){
   fitDataList[eventNo].trigCH2 = f2.GetParameter(5);
   fitDataList[eventNo].bslnCH2 = f2.GetParameter(6);
 
-  //std::cout << "Finished storing parameters into fitDataList\n";
+  //std::cout << "Finished storing parameters into fitDataList\n"; //yippee!
 
+  //save pngs of events with fit data
+  //please make them lok nicer if you want, they're super bare bones now...
   if(savePngs){
     
-    TCanvas *c = new TCanvas(Form("c_event_%d", eventNo), "", 1000, 700);
+    TCanvas *c = new TCanvas(Form("c_event_%d", eventNo), "", 1000, 700); //create a canvas to hold out graph and accessories 
 
-    g1.SetTitle(Form("Event %d;Time (s);Voltage (V)", eventNo));
+    g1.SetTitle(Form("Event %d;Time (s);Voltage (V)", eventNo)); //title
 
+	//visuals for CH1 data
     g1.SetMarkerStyle(20);
     g1.SetMarkerSize(0.5);
     g1.SetLineColor(kBlue);
@@ -484,6 +517,7 @@ void computeFitEvent(int eventNo){
     g2.SetLineColor(kRed);
     g2.SetMarkerColor(kRed);
 
+	//visuals for CH1 fit
     f1.SetLineColor(kBlue + 2);
     f1.SetLineWidth(2);
 
@@ -496,6 +530,7 @@ void computeFitEvent(int eventNo){
     f1.Draw("SAME");
     f2.Draw("SAME");
 
+	//key for distinguishing series
     TLegend *leg = new TLegend(0.65, 0.72, 0.88, 0.88);
     leg->AddEntry(&g1, "CH1 data", "p");
     leg->AddEntry(&f1, "CH1 fit", "l");
@@ -503,7 +538,7 @@ void computeFitEvent(int eventNo){
     leg->AddEntry(&f2, "CH2 fit", "l");
     leg->Draw();
 
-    c->SaveAs(Form("%d.png", eventNo));
+    c->SaveAs(Form("%d.png", eventNo)); //save as png
 
     delete leg;
     delete c;
@@ -512,25 +547,28 @@ void computeFitEvent(int eventNo){
 }
 
 
-int RPBinDecoder(){
+//this is basically the "int main{}" of root if you're familiar with C++
+//if you ever change the name of the file from "RPBinDecoderV1.cc", you have to adjust the "main" accordingly
+int RPBinDecoderV1(){
 
 
   if((eventsPerFile > 1e3) && savePngs){
-    std::cout << "Why are you saving " << eventsPerFile << " pngs?\n";
+    std::cout << "Why are you saving " << eventsPerFile << " pngs?\n"; //valid question
     return 1;
   }
 
-  for(int runNo = 1; runNo < 2; ++runNo){// runNo > 851
+  for(int runNo = 1; runNo < 2; ++runNo){ //this allows you to analyze data across multuple runs, by default will only analyze run1 as stated
 
+	//clear data lists before fetching new file
     outDataList.clear();
     fitDataList.clear();
 
-    std::string curFilename = "/home/bhartsock/RP/RPBinDecoder/data/22Na/";
+    std::string curFilename = "/home/bhartsock/RP/RPBinDecoder/data/22Na/"; //file path prefix expecting subdirectory of "run1.bin, run2.bin, ..."
     curFilename += "run";
     curFilename += std::to_string(runNo);
     curFilename += ".bin";
   
-    if(!readStoreData(curFilename)){
+    if(!readStoreData(curFilename)){ //if can't open file
       std::cout << "Can't open " << curFilename << "\n";
       return 1;
     }
@@ -552,13 +590,14 @@ int RPBinDecoder(){
 
     //std::cout << "Finished fits, outputting data\n";
 
+	//output data and fit data filename
     std::string outFilename = "22Na";
     outFilename += std::to_string(runNo) + ".root";
 
     storeDataInNtuple(outFilename, outDataList, fitDataList);
 
   }
-  std::cout << "Hey good job, you requested enough memory :)\n";
+  std::cout << "Hey good job, you requested enough memory :)\n"; //:)
  
   return 0;
 }
